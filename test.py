41f0010d0d7286f44856e3f433fc40f07ee529d7
Stuart Roy <stuart.roy@genesisoilandgas.com>
2020-01-31 12:14:39 +0000

    Parses ppl directly to hdf5, however, this creates a tight coupling between ppl files and an hdf5 file. Suggest a looser coupling with a lightweight PPL datastructure (or dataclass) that has a to_hdf5() method

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import re
+import sys
 import datetime
 from dateutil import parser as parser
 from dataclasses import dataclass, field
@@ -11,8 +12,160 @@ import h5py
 
 re_num = r"[\+|-]?\d*\.*\d+(?:[e|E][\+|-]?\d+)?"
 
+ppl_regex_strings = [
+    r"\'(OLGA) ([\d+.]*\d)\'",
+    r"(INPUT FILE)\n\'(.*)\'",
+    r"(PVT FILE)\n\'(.*)\'",
+    r"(RESTART FILE)\n\'(.*)\'",
+    r"(DATE)\n\'(.*)\'",
+    r"(PROJECT)\n\'(.*)\'",
+    r"(TITLE)\n\'(.*)\'",
+    r"(AUTHOR)\n\'(.*)\'",
+    f"(NETWORK)\n({re_num})",
+    r"(GEOMETRY)\s*\'\s*\((.*)\)\s*\'",
+    rf"(BRANCH)\n\'(.*)\'\n(\d*)((?:\s*{re_num})*)",
+    r"(CATALOG)\s*(\d*)((?:\n.+\s*\'(?:BOUNDARY|SECTION):\'\s*\'BRANCH:\'\s*\'.*\'\s*\'\(.*\)\'\s*\'.*\')*)",
+    rf"(TIME SERIES)\s*\'\s*\((.*)\)\s*\'((?:\s*{re_num})*)",
+]
+
+
 def ppl_to_hdf5(ppl_file_path, hdf5_file_name):
-    pass
+    ppl_matches = parse_ppl_file(ppl_file_path)
+    with h5py.File(f"{hdf5_file_name}.h5", "w") as hdf5:
+        build_hdf5_from_regex_matches(hdf5, ppl_matches)
+
+
+def parse_ppl_file(ppl_file_path):
+    regex = re.compile("|".join(ppl_regex_strings))
+    with open(ppl_file_path, "r") as f:
+        data = f.read()
+    matches = [tuple(v for v in m if v != "") for m in regex.findall(data)]
+    return matches
+
+
+keywords = [
+    "olga",
+    "input_file",
+    "pvt_file",
+    "restart_file",
+    "date",
+    "project",
+    "title",
+    "author",
+    "network",
+    "geometry",
+]
+
+
+def build_hdf5_from_regex_matches(hdf5_file, matches):
+    for match in matches:
+        keyword = match[0].lower().replace(" ", "_")
+        if keyword in keywords:
+            hdf5_file.attrs.create(keyword, match[1])
+    branches = [match for match in matches if match[0].lower() == "branch"]
+    catalog = [match for match in matches if match[0].lower() == "catalog"][0]
+    time_series = [match for match in matches if match[0].lower() == "time series"][0]
+    add_time_series_data_to_hdf5_file(hdf5_file, branches, catalog, time_series)
+
+
+def add_time_series_data_to_hdf5_file(hdf5_file, branches, catalog, time_series):
+    create_branches_in_hdf5_file(hdf5_file, branches)
+    add_ts_as_dataframes(hdf5_file, catalog, time_series)
+
+
+def create_branches_in_hdf5_file(hdf5_file, branches):
+    for branch in branches:
+        geometry = pd.DataFrame(
+            np.reshape(branch[3].strip().split(), (2, int(branch[2]) + 1)).T.astype(
+                np.float64
+            ),
+            columns=["Length", "Elevation"],
+        )
+        hdf5_file.create_group(branch[1])
+        hdf5_file[f"/{branch[1]}"].attrs.create("geometry", geometry)
+
+
+def add_ts_as_dataframes(hdf5_file, catalog, time_series):
+    catalog, time_steps = build_catalog_with_ts(catalog, time_series)
+    hdf5_file.attrs.create("time_steps", np.array(time_steps))
+    add_ts_to_branches(hdf5_file, catalog)
+
+def build_catalog_with_ts(catalog, time_series):
+    cat = build_catalog(catalog)
+    cat_inc = len(cat) + 1
+    time_steps = []
+    for n, v in enumerate(time_series[2].strip().split("\n")):
+        row = n % cat_inc
+        if row == 0:
+            time_steps.append(float(v))
+        else:
+            cat[row - 1]['time_series'].append(np.array(v.split(), dtype=np.float))
+    return cat, time_steps
+
+def build_catalog(catalog):
+    p = re.compile(
+        r"(.*) \'(SECTION|BOUNDARY):\' \'BRANCH:\' \'(.*)\' \'\((.*)\)\' \'(.*)\'"
+    )
+    cat = []
+    cat_list = catalog[2].strip().split("\n")
+    for c in cat_list:
+        m = p.findall(c)[0]
+        cat.append({"symbol": m[0], "kind": m[1], "branch": m[2], "units": m[3], "description": m[4], "time_series": []})
+    return cat
+
+def add_ts_to_branches(hdf5_file, catalog):
+    for branch in hdf5_file.keys():
+        print(hdf5_file[f"/{branch}"].attrs.get("geometry"))
+
+
+
+def _generate_branch_data(self):
+    for c in self.catalog:
+        self.branches[c.branch].catalogs.append(c)
+
+    times = len(self.time_series)
+    for _, branch in self.branches.items():
+        nodes = branch.geometry.shape[0]
+        sections = len([c for c in branch.catalogs if c.kind == "SECTION"])
+        s = np.zeros((nodes - 1, times, sections))
+        b = np.zeros((nodes, times, len(branch.catalogs) - sections))
+        i_bound = i_sec = 0
+        for cat in branch.catalogs:
+            if cat.kind == "SECTION":
+                s[:, :, i_sec] = np.array(cat.time_series).T
+                i_sec += 1
+            else:
+                b[:, :, i_bound] = np.array(cat.time_series).T
+                i_bound += 1
+
+        b = b.reshape((np.prod(b.shape[:-1]), b.shape[-1]))
+        midx_b = pd.MultiIndex.from_product(
+            [branch.geometry["Length"], self.time_series]
+        )
+        branch.boundaries = pd.DataFrame(
+            data=b,
+            index=midx_b,
+            columns=[c.symbol for c in branch.catalogs if c.kind == "BOUNDARY"],
+        )
+
+        s = s.reshape((np.prod(s.shape[:-1]), s.shape[-1]))
+        midx_s = pd.MultiIndex.from_product(
+            [branch.geometry["Length"][:-1], self.time_series]
+        )
+        branch.sections = pd.DataFrame(
+            data=s,
+            index=midx_s,
+            columns=[c.symbol for c in branch.catalogs if c.kind == "SECTION"],
+        )
+
+
+ppl_to_hdf5("tests\\test_files\\SD1H_35.ppl", "test")
+hdf = h5py.File("test.h5", "r")
+print(hdf.keys())
+hdf.close()
+
+
+"""
 
 
 class PPLReader:
@@ -219,3 +372,4 @@ ppl.to_hdf5("test")
 hdf = h5py.File("test.h5", 'r')
 print(hdf.attrs["olga"])
 hdf.close()
+"""
